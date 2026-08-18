0b0e3c2e47dba2c438e2aa061b9e37cd61e9e51e
Antto Halme <antto.halme@gmail.com>
2026-08-19 00:30:05 +0300

    Added the file(s)

---
+++ moduuli 2/numerolukot.py
@@ -0,0 +1,4 @@
+from random import randint
+
+print(f"Lukon koodi: {randint(0, 9)}{randint(0, 9)}{randint(0, 9)}")
+print(f"Lukon koodi: {randint(1, 6)}{randint(1, 6)}{randint(1, 6)}{randint(1, 6)}")
