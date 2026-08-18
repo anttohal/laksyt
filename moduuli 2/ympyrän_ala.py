66b3f5131a150f826b20a518aee5f0c178dbce4a
Antto Halme <antto.halme@gmail.com>
2026-08-18 13:34:14 +0300

    Added the files

---
+++ moduuli 2/ympyrän_ala.py
@@ -0,0 +1,8 @@
+import math
+
+# Luetaan syöte
+r = float(input('Anna ympyrän säde: '))
+
+A = math.pi * pow(r, 2)
+
+print(f"Pinta-ala on {A:.2f}")
--- moduuli 3/kuhan_pituus.py
+++ moduuli 3/kuhan_pituus.py
@@ -2,5 +2,4 @@ minimi_pituus = 37
 pituus = float(input('Anna pituus: '))
 
 if pituus < minimi_pituus:
-
     print(f"Laske kalav takaisin! Se on {minimi_pituus - pituus:.2f} senttiä liian lyhyt.")
