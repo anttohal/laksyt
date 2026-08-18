895bbc5b864991192ed1b1c1c026c5d155fc0be1
Antto Halme <antto.halme@gmail.com>
2026-08-18 13:27:02 +0300

    Added the files

---
+++ moduuli 3/kuhan_pituus.py
@@ -0,0 +1,6 @@
+minimi_pituus = 37
+pituus = float(input('Anna pituus: '))
+
+if pituus < minimi_pituus:
+
+    print(f"Laske kalav takaisin! Se on {minimi_pituus - pituus:.2f} senttiä liian lyhyt.")
